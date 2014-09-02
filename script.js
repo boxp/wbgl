1b98f0b4347447926d26e69245f3500cdea3c21d
Ketaro Takeuchi <tiyotiyouda@gmail.com>
2014-09-02 15:16:23 +0900

    finished 25

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 window.onload = function () {
 
   var c = document.getElementById('canvas');
@@ -54,11 +53,21 @@ window.onload = function () {
   // 環境光の色
   var ambientColor = [0.1, 0.1, 0.1, 0.1];
 
-  var torusData = torus(32, 32, 1.0, 2.0);
+  var torusData = torus(32, 32, 0.5, 1.0);
   vertex_position = torusData[0];
   vertex_normal = torusData[1]
   vertex_color = torusData[2];
   ibo_index = torusData[3];
+
+  // 球体の頂点データからVBOを生成し配列に格納
+  var sphereData = sphere(64, 64, 2.0, [0.25, 0.25, 0.75, 1.0]);
+  var sPositon = create_vbo(sphereData.p);
+  var sNormal = create_vbo(sphereData.n);
+  var sColor = create_vbo(sphereData.c);
+  var sVBOList = [sPositon, sNormal, sColor];
+
+  // 球体用IBO作成
+  var sIndex = create_ibo(sphereData.i);
 
   // VBOを生成
   var vbo = new Array(3);
@@ -86,21 +95,22 @@ window.onload = function () {
 
   var uniLocation = new Array();
   uniLocation[0] = gl.getUniformLocation(prg, 'mvpMatrix');
-  uniLocation[1] = gl.getUniformLocation(prg, 'invMatrix');
-  uniLocation[2] = gl.getUniformLocation(prg, 'lightDirection');
-  uniLocation[3] = gl.getUniformLocation(prg, 'eyeDirection');
-  uniLocation[4] = gl.getUniformLocation(prg, 'ambientColor');
+  uniLocation[1] = gl.getUniformLocation(prg, 'mMatrix');
+  uniLocation[2] = gl.getUniformLocation(prg, 'invMatrix');
+  uniLocation[3] = gl.getUniformLocation(prg, 'lightPosition');
+  uniLocation[4] = gl.getUniformLocation(prg, 'eyeDirection');
+  uniLocation[5] = gl.getUniformLocation(prg, 'ambientColor');
 
   // 視点の向き
   var eyeDirection = [0.0, 0.0, 10.0];
 
   // ビュー座標変換行列
   m.lookAt(eyeDirection, [0, 0, 0], [0, 1, 0], vMatrix);
-  m.perspective(90, c.width / c.height, 0.1, 100, pMatrix);
+  m.perspective(60, c.width / c.height, 0.1, 100, pMatrix);
   m.multiply(pMatrix, vMatrix, tmpMatrix);
 
   // 平行光源の向き
-  var lightDirection = [-0.5, 0.5, 0.5];
+  var lightPosition = [0.0, 0.0, 0.0];
 
   // カウンタ
   var count = 0;
@@ -176,21 +186,48 @@ window.onload = function () {
     // カウンタをインクリメント
     count++;
 
-    // カウンタを元にラジアン(0~359)を取得
+    // カウンタを元にラジアン(0~359)と各種座標を取得
     var rad = (count % 360) * Math.PI / 180;
+    var tx = Math.cos(rad) * 3.5;
+    var ty = Math.sin(rad) * 3.5;
+    var tz = Math.sin(rad) * 3.5;
+
+    // トーラスのIBOとVBOを設定
+    set_attribute(vbo, attLocation, attStride);
+
+    // IBOをバインドし、登録する
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
 
     m.identity(mMatrix);
-    // x軸を中心に回転させます
-    m.rotate(mMatrix, rad, [1, 0, 0], mMatrix);
+    m.translate(mMatrix, [tx, -ty, -tz], mMatrix);
+    m.rotate(mMatrix, -rad, [0, 1, 1], mMatrix);
     m.multiply(tmpMatrix, mMatrix, mvpMatrix);
-    gl.uniformMatrix4fv(uniLocation[0], false, mvpMatrix);
-    gl.uniformMatrix4fv(uniLocation[1], false, invMatrix);
-    gl.uniform3fv(uniLocation[2], lightDirection);
-    gl.uniform3fv(uniLocation[3], eyeDirection);
-    gl.uniform4fv(uniLocation[4], ambientColor);
+    m.inverse(mMatrix, invMatrix);
 
-    // インデックスを用いた描画命令
+    // uniform変数の登録と描画
+    gl.uniformMatrix4fv(uniLocation[0], false, mvpMatrix);
+    gl.uniformMatrix4fv(uniLocation[1], false, mMatrix);
+    gl.uniformMatrix4fv(uniLocation[2], false, invMatrix);
+    gl.uniform3fv(uniLocation[3], lightPosition);
+    gl.uniform3fv(uniLocation[4], eyeDirection);
+    gl.uniform4fv(uniLocation[5], ambientColor);
     gl.drawElements(gl.TRIANGLES, ibo_index.length, gl.UNSIGNED_SHORT, 0);
+
+    // 球体のVBO,IBOをセット
+    set_attribute(sVBOList, attLocation, attStride);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, sIndex);
+
+    // モデル座標変換行列の生成
+    m.identity(mMatrix);
+    m.translate(mMatrix, [-tx, ty, tz], mMatrix);
+    m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+    m.inverse(mMatrix, invMatrix);
+
+    // uniform変数の登録と描画
+    gl.uniformMatrix4fv(uniLocation[0], false, mvpMatrix);
+    gl.uniformMatrix4fv(uniLocation[1], false, mMatrix);
+    gl.uniformMatrix4fv(uniLocation[2], false, invMatrix);
+    gl.drawElements(gl.TRIANGLES, sphereData.i.length, gl.UNSIGNED_SHORT, 0);
 
     // コンテキストの再描画
     gl.flush();
@@ -301,4 +338,40 @@ window.onload = function () {
     return vbo;
   }
 
+}
+
+// 球体を生成する関数
+function sphere(row, column, rad, color){
+    var pos = new Array(), nor = new Array(),
+        col = new Array(), idx = new Array();
+    for(var i = 0; i <= row; i++){
+        var r = Math.PI / row * i;
+        var ry = Math.cos(r);
+        var rr = Math.sin(r);
+        for(var ii = 0; ii <= column; ii++){
+            var tr = Math.PI * 2 / column * ii;
+            var tx = rr * rad * Math.cos(tr);
+            var ty = ry * rad;
+            var tz = rr * rad * Math.sin(tr);
+            var rx = rr * Math.cos(tr);
+            var rz = rr * Math.sin(tr);
+            if(color){
+                var tc = color;
+            }else{
+                tc = hsva(360 / row * i, 1, 1, 1);
+            }
+            pos.push(tx, ty, tz);
+            nor.push(rx, ry, rz);
+            col.push(tc[0], tc[1], tc[2], tc[3]);
+        }
+    }
+    r = 0;
+    for(i = 0; i < row; i++){
+        for(ii = 0; ii < column; ii++){
+            r = (column + 1) * i + ii;
+            idx.push(r, r + 1, r + column + 2);
+            idx.push(r, r + column + 2, r + column + 1);
+        }
+    }
+    return {p : pos, n : nor, c : col, i : idx};
 }
